418bb274134347581111fb38e413de4fb5c7fcee
Matthew Solt <mrsolt@stanford.edu>
2017-10-12 17:59:26 -0700

    adding photon position tuple maker

---
+++ src/main/java/org/hps/users/mrsolt/PhotonPositionDriver.java
@@ -0,0 +1,43 @@
+package org.hps.users.mrsolt;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.lcsim.event.EventHeader;
+import org.lcsim.event.MCParticle;
+
+/**
+ * This is the tuple template driver
+ * Use this to add your code and variables to make a tuple
+ * Run the GeneralTupleDriver to output info into a text file
+ * Change the steering file to include this driver
+ * Run "makeTree.py" on text file to create a root tuple
+ *
+ * @author mrsolt on Aug 31, 2017
+ */
+
+public class PhotonPositionDriver extends GeneralTupleDriverPhotonPosition {
+	
+    public static void fillVariables(EventHeader event, MCParticle p) {
+        
+        if(p.getPDGID() == 22){
+        	tupleMap.put("run/I", (double) event.getRunNumber());
+            tupleMap.put("event/I", (double) event.getEventNumber());
+            tupleMap.put("energy/D",p.getEnergy());
+        	tupleMap.put("startX/D",p.getOrigin().x());
+        	tupleMap.put("startY/D",p.getOrigin().y());
+        	tupleMap.put("startZ/D",p.getOrigin().z());
+        	tupleMap.put("Px/D",p.getPX());
+        	tupleMap.put("Py/D",p.getPY());
+        	tupleMap.put("Pz/D",p.getPZ());
+        }
+    }
+    
+    public static void addVariables() {
+    	
+        String[] newVars = new String[]{"run/I", "event/I",
+        		"energy/D","startX/D","startY/D","startZ/D","endX/D","endY/D","endZ/D",
+        		"Px/D","Py/D","Pz/D","parentID/I"};
+        tupleVariables.addAll(Arrays.asList(newVars));
+    }
+}
