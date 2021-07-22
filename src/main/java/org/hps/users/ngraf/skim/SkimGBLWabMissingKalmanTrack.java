69792e535bdcf977ceac7083a3afeba44dca146d
Graf <ngraf@slac.stanford.edu>
2021-07-21 20:31:59 -0700

    Update SkimGBLWabMissingKalmanTrack.java
    Add a few diagnostic histograms to demonstrate quality of selection.

--- src/main/java/org/hps/users/ngraf/skim/SkimGBLWabMissingKalmanTrack.java
+++ src/main/java/org/hps/users/ngraf/skim/SkimGBLWabMissingKalmanTrack.java
@@ -5,6 +5,7 @@ import org.lcsim.event.Cluster;
 import org.lcsim.event.EventHeader;
 import org.lcsim.event.ReconstructedParticle;
 import org.lcsim.util.Driver;
+import org.lcsim.util.aida.AIDA;
 
 /**
  * Select clean WAB candidates missing a Kalman Track
@@ -13,6 +14,7 @@ import org.lcsim.util.Driver;
  */
 public class SkimGBLWabMissingKalmanTrack extends Driver {
 
+    private AIDA aida = AIDA.defaultInstance();
     private int _numberOfEventsSelected;
     private int _numberOfEventsProcessed = 0;
 
@@ -27,6 +29,17 @@ public class SkimGBLWabMissingKalmanTrack extends Driver {
             if (rpListGBL.size() == 2) {
                 if (rpListGBL.get(0).getParticleIDUsed().getPDG() == 11 && rpListGBL.get(1).getParticleIDUsed().getPDG() == 22) {
                     Cluster electronCluster = rpListGBL.get(0).getClusters().get(0);
+                    double electronmomentum = rpListGBL.get(0).getMomentum().magnitude();
+                    Cluster photonCluster = rpListGBL.get(1).getClusters().get(0);
+                    // check on energy sum and energy momentum sum
+                    double esum = electronCluster.getEnergy() + photonCluster.getEnergy();
+                    double epsum = electronmomentum + photonCluster.getEnergy();
+                    aida.histogram1D("wab e1 + e2", 100, 0., 5.).fill(esum);
+                    aida.histogram1D("wab p1 + e2", 100, 0., 5.).fill(epsum);
+                    String half = electronCluster.getPosition()[1] < 0 ? " bottom " : " top ";
+                    aida.histogram1D("wab e1 + e2 electron " + half, 100, 0., 5.).fill(esum);
+                    aida.histogram1D("wab p1 + e2 electron " + half, 100, 0., 5.).fill(epsum);
+
                     List<ReconstructedParticle> rpListKF = event.get(ReconstructedParticle.class, "FinalStateParticles_KF");
                     boolean electronClusterHasKfTrack = false;
                     //check whether any electron built from a KF track is associated to the GBL track electron cluster
