5b74d67c284f2b2473766e0973c286c287fdfe5b
Norman Graf <Norman.Graf@slac.stanford.edu>
2021-10-16 01:56:57 -0400

    Removed calorimeter cluster info: track only

--- src/main/java/org/hps/users/ngraf/dataanalysis/TrackAnalysis2021.java
+++ src/main/java/org/hps/users/ngraf/dataanalysis/TrackAnalysis2021.java
@@ -39,65 +39,68 @@ public class TrackAnalysis2021 extends Driver {
                     // only consider tracks with all sensors hit
                     if (rp.getTracks().get(0).getTrackerHits().size() >= 12) {
                         //only consider particles with associated cluster
-                        if (!rp.getClusters().isEmpty()) {
-                            String type = pdgId == 11 ? " electron" : " positron";
+                        //20211016 remove any cluster requirement,as we are most interested in the channels close to the beam
+                        // requiring a cluster eliminates small-angle tracks
+//                        if (!rp.getClusters().isEmpty()) {
+                        String type = pdgId == 11 ? " electron" : " positron";
 
-                            Cluster c = rp.getClusters().get(0);
-                            Track t = rp.getTracks().get(0);
-                            double p = rp.getMomentum().magnitude();
-                            double e = rp.getEnergy();
-                            if (e > 0.5) {
-                                aida.tree().mkdirs(type + " Analysis");
-                                aida.tree().cd(type + " Analysis");
-                                CalorimeterHit seed = c.getCalorimeterHits().get(0);
-                                int ix = seed.getIdentifierFieldValue("ix");
-                                int iy = seed.getIdentifierFieldValue("iy");
-                                String fid = TriggerModule.inFiducialRegion(c) ? " fiducial " : " non fid ";
-                                String topOrBottom = iy > 0 ? " top " : " bottom ";
-                                aida.histogram2D("cluster ix vs iy" + type, 47, -23.5, 23.5, 11, -5.5, 5.5).fill(ix, iy);
+//                            Cluster c = rp.getClusters().get(0);
+                        Track t = rp.getTracks().get(0);
+                        double p = rp.getMomentum().magnitude();
+                        String topOrBottom = rp.getMomentum().y() > 0 ? " top " : " bottom ";
+                        double e = rp.getEnergy();
+                        if (e > 0.5) {
+                            aida.tree().mkdirs(type + " Analysis");
+                            aida.tree().cd(type + " Analysis");
+//                                CalorimeterHit seed = c.getCalorimeterHits().get(0);
+//                                int ix = seed.getIdentifierFieldValue("ix");
+//                                int iy = seed.getIdentifierFieldValue("iy");
+//                                String fid = TriggerModule.inFiducialRegion(c) ? " fiducial " : " non fid ";
+//                                String topOrBottom = iy > 0 ? " top " : " bottom ";
+//                                aida.histogram2D("cluster ix vs iy" + type, 47, -23.5, 23.5, 11, -5.5, 5.5).fill(ix, iy);
 //                                aida.histogram1D("EoverP ix: " + ix + " iy: " + iy + type, 100, 0., 2.0).fill(e / p);
-                                aida.histogram1D("Track momentum" + topOrBottom + type, 200, 0., 7.0).fill(p);
-                                aida.histogram2D("E vs p" + topOrBottom + type, 100, 0., 7., 100, 0., 7.).fill(e, p);
-                                aida.histogram2D("E vs p" + topOrBottom + fid + type, 100, 0., 7., 100, 0., 7.).fill(e, p);
-                                aida.histogram1D("EoverP" + topOrBottom + fid + type, 100, 0., 2.0).fill(e / p);
+                            aida.histogram1D("Track momentum" + topOrBottom + type, 200, 0., 7.0).fill(p);
+//                                aida.histogram2D("E vs p" + topOrBottom + type, 100, 0., 7., 100, 0., 7.).fill(e, p);
+//                                aida.histogram2D("E vs p" + topOrBottom + fid + type, 100, 0., 7., 100, 0., 7.).fill(e, p);
+//                                aida.histogram1D("EoverP" + topOrBottom + fid + type, 100, 0., 2.0).fill(e / p);
 
-                                setupSensors(event);
-                                List<TrackerHit> hits = t.getTrackerHits();
-                                for (TrackerHit hit : hits) {
-                                    HpsSiSensor sensor = (HpsSiSensor) ((RawTrackerHit) hit.getRawHits().get(0)).getDetectorElement();
-                                    String sensorName = sensor.getName();
-                                    int layer = sensor.getLayerNumber();
-                                    String sensorType = layer < 3 ? "thin" : "thick";
-                                    int clusSize = hit.getRawHits().size();
+                            setupSensors(event);
+                            List<TrackerHit> hits = t.getTrackerHits();
+                            for (TrackerHit hit : hits) {
+                                HpsSiSensor sensor = (HpsSiSensor) ((RawTrackerHit) hit.getRawHits().get(0)).getDetectorElement();
+                                String sensorName = sensor.getName();
+                                int layer = sensor.getLayerNumber();
+                                String sensorType = layer < 3 ? "thin" : "thick";
+                                int clusSize = hit.getRawHits().size();
 //                                    double dEdx = 1000000 * hit.getdEdx();
-                                    double cluAmp = hit.getdEdx()/ENERGY_EHPAIR;
-                                    short[] adcVals = ((RawTrackerHit) hit.getRawHits().get(0)).getADCValues();
-                                    double time = hit.getTime();
+                                double cluAmp = hit.getdEdx() / ENERGY_EHPAIR;
+                                short[] adcVals = ((RawTrackerHit) hit.getRawHits().get(0)).getADCValues();
+                                double time = hit.getTime();
 //                                    aida.histogram1D("hit dEdx " + sensorType, 100, 0., 20.).fill(dEdx);
-                                    aida.histogram1D("hit corrected cluster amplitude " + sensorType, 100, 0., 3000.).fill(cluAmp);
-                                    aida.histogram1D("hit time " + sensorType, 100, -20., 20.).fill(time);
+                                aida.histogram1D("hit corrected cluster amplitude " + sensorType, 100, 0., 3000.).fill(cluAmp);
+                                aida.histogram1D("hit time " + sensorType, 100, -20., 20.).fill(time);
 //                                    aida.histogram1D("hit cluster size " + sensorName, 10, 0.5, 10.5).fill(clusSize);
-                                    if (type.equals(" electron") && !sensorName.contains("slot")) {
-                                        aida.histogram1D(sensorName + " hit corrected cluster amplitude", 100, 0., 3000.).fill(cluAmp);
-                                        if (clusSize < 3) {
-                                            aida.histogram1D(sensorName + " hit corrected cluster amplitude " + clusSize + " strip cluster", 100, 0., 3000.).fill(cluAmp);
-                                        }
-                                        aida.histogram1D(sensorName + " cluster size", 10, -0.5, 9.5).fill(clusSize);
+                                if (type.equals(" electron") && !sensorName.contains("slot")) {
+                                    aida.histogram1D(sensorName + " hit corrected cluster amplitude", 100, 0., 3000.).fill(cluAmp);
+                                    if (clusSize < 3) {
+                                        aida.histogram1D(sensorName + " hit corrected cluster amplitude " + clusSize + " strip cluster", 100, 0., 3000.).fill(cluAmp);
                                     }
-                                    if (type.equals(" positron") && !sensorName.contains("hole")) {
-                                        aida.histogram1D(sensorName + " hit corrected cluster amplitude", 100, 0., 3000.).fill(cluAmp);
-                                        if (clusSize < 3) {
-                                            aida.histogram1D(sensorName + " hit corrected cluster amplitude " + clusSize + " strip cluster", 100, 0., 3000.).fill(cluAmp);
-                                        }
-                                        aida.histogram1D(sensorName + " cluster size", 10, -0.5, 9.5).fill(clusSize);
+                                    aida.histogram1D(sensorName + " cluster size", 10, -0.5, 9.5).fill(clusSize);
+                                }
+                                if (type.equals(" positron") && !sensorName.contains("hole")) {
+                                    aida.histogram1D(sensorName + " hit corrected cluster amplitude", 100, 0., 3000.).fill(cluAmp);
+                                    if (clusSize < 3) {
+                                        aida.histogram1D(sensorName + " hit corrected cluster amplitude " + clusSize + " strip cluster", 100, 0., 3000.).fill(cluAmp);
                                     }
-                                    // let's look at some individual strips, so select single strip clusters.
-                                    if (clusSize == 1) {
-                                        List<RawTrackerHit> clusterRawHits = hit.getRawHits();
-                                        int channel = clusterRawHits.get(0).getIdentifierFieldValue("strip");
-                                        aida.histogram1D(sensor.getName() + " channel", 640, 0, 640.).fill(channel);
-                                        aida.histogram2D(sensor.getName() + " cluster amplitude vs channel", 640, 0, 640., 100, 0., 3000.).fill(channel, cluAmp);
-                                        
+                                    aida.histogram1D(sensorName + " cluster size", 10, -0.5, 9.5).fill(clusSize);
+                                }
+                                // let's look at some individual strips, so select single strip clusters.
+                                if (clusSize == 1) {
+                                    List<RawTrackerHit> clusterRawHits = hit.getRawHits();
+                                    int channel = clusterRawHits.get(0).getIdentifierFieldValue("strip");
+                                    aida.histogram1D(sensor.getName() + " channel", 640, 0, 640.).fill(channel);
+                                    aida.histogram2D(sensor.getName() + " cluster amplitude vs channel", 640, 0, 640., 100, 0., 3000.).fill(channel, cluAmp);
+
 //                                        //let's start with the split-strip sensors...
 //                                        if (sensorName.contains("L1") || sensorName.contains("L2")) {
 //                                            if (channel < 20 || channel > 490) {
@@ -107,14 +110,15 @@ public class TrackAnalysis2021 extends Driver {
 //                                                aida.tree().cd("..");
 //                                            }
 //                                        }
-                                    }
+                                }
 //                                    aida.histogram1D(sensorName + " hit time", 100, -20., 20.).fill(time);
 //                                    aida.histogram1D(sensorName + " hit cluster size", 10, -0.5, 9.5).fill(clusSize);
 //                                    aida.histogram2D(sensorName + " dEdx vs time", 100, 0., 20., 100, -20., 20.).fill(dEdx, time);
-                                }
-                                aida.tree().cd("..");
                             }
+                            aida.tree().cd("..");
+//                            }
                         }
+//                    }
                     }
                 }
             }
